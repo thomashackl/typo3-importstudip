b9f69e9c77ed1fbc2953dab2b80e8e061c0e45ff
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-07-23 13:41:32 +0200

    cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,14 +22,14 @@ $EM_CONF[$_EXTKEY] = array(
     'conflicts' => '',
     'priority' => '',
     'module' => '',
-    'state' => 'stable',
+    'state' => 'alpha',
     'internal' => '',
     'uploadfolder' => 0,
     'createDirs' => '',
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'author_company' => '',
+    'author_company' => 'University of Passau',
     'version' => '3.0.0',
     'constraints' => array(
         'depends' => array(
@@ -39,10 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
         ),
         'suggests' => array(
         ),
-    ),
-    '_md5_values_when_last_written' => 'a:28:{s:9:"ChangeLog";s:4:"ef7f";s:21:"ext_conf_template.txt";s:4:"a269";s:12:"ext_icon.gif";s:4:"d02a";s:17:"ext_localconf.php";s:4:"67af";s:14:"ext_tables.php";s:4:"b6a3";s:13:"locallang.xml";s:4:"e900";s:10:"README.txt";s:4:"95d7";s:7:"tca.php";s:4:"c8ba";s:19:"doc/wizard_form.dat";s:4:"1b0c";s:20:"doc/wizard_form.html";s:4:"74bb";s:14:"pi1/ce_wiz.gif";s:4:"cf98";s:14:"pi1/ce_wiz.png";s:4:"1d09";s:33:"pi1/class.tx_importstudip_pi1.php";s:4:"cd11";s:41:"pi1/class.tx_importstudip_pi1_wizicon.php";s:4:"2683";s:13:"pi1/clear.gif";s:4:"cc11";s:23:"pi1/flexform_ds_pi1.xml";s:4:"ca42";s:17:"pi1/locallang.xml";s:4:"2009";s:24:"pi1/static/editorcfg.txt";s:4:"46a1";s:38:"queue/170ffbdc9ccf9145cc22769f2006e81c";s:4:"08d2";s:38:"queue/67efa2369a3f9284f529b9e952f05b55";s:4:"c05e";s:38:"queue/93fdf3562ba1a5398e7132c88a2bd428";s:4:"6511";s:38:"queue/extern.php?module=Lecturedetails";s:4:"9a46";s:39:"res/class.tx_importstudip_semtreeid.php";s:4:"074e";s:34:"res/class.tx_importstudip_soap.php";s:4:"ea6a";s:45:"res/class.tx_importstudip_studipconnector.php";s:4:"cfc6";s:45:"res/class.tx_importstudip_subjectarealist.php";s:4:"2930";s:21:"res/functions.inc.php";s:4:"5ca4";s:15:"res/intelec.png";s:4:"b3e0";}',
-    'suggests' => array(
-    ),
+    )
 );
 
 ?>
