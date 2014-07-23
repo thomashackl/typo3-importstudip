160ce37d479a9f6764ae9a279ba1951490cf2ae5
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-07-23 10:57:50 +0200

    namespaces and flexform inclusion

--- ext_tables.php
+++ ext_tables.php
@@ -1,18 +1,24 @@
 <?php
-Tx_Extbase_Utility_Extension::registerPlugin(
+
+use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use \TYPO3\CMS\Core\Utility\GeneralUtility;
+use \TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
+// Register plugin for usage.
+ExtensionUtility::registerPlugin(
     $_EXTKEY,
     'Pi1',
     'LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang.xlf:tx_importstudip.plugintitle'
 );
 
-$extensionName = t3lib_div::underscoredToUpperCamelCase($_EXTKEY);
+// Include Flexform
+$extensionName = strtolower(GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
 $pluginSignature = strtolower($extensionName) . '_pi1';
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,recursive';
-
-// Flexform
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY.'/Configuration/FlexForms/flexform.xml');
+ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/flexform.xml');
 
+// Wizicon
 if (TYPO3_MODE == 'BE') {
-    $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['UniPassau\\ImportStudip\\Wizicon'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Wizicon.php';
+    $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['UniPassau\\ImportStudip\\Wizicon'] = ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Wizicon.php';
 }
