973bddbb198ff1c28bdebe6bef93a47925de7aa8
Thomas Hackl <thomas.hackl@uni-passau.de>
2015-09-07 13:41:11 +0200

    remove require_once in favor of correct namespacing and TYPO3 autoloader

---
+++ ext_autoload.php
@@ -0,0 +1,5 @@
+<?php
+$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('importstudip');
+return array(
+    'restclient' => $extensionPath . 'Resources/Private/PHP/restclient/restclient.php'
+);
