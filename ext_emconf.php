304063676d049ad4601f752f61a2fcdc4906bceb
Thomas Hackl <thomas.hackl@uni-passau.de>
2020-11-30 09:54:14 +0100

    don't include frontend CSS anymore, should be set in global CSS for extension

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,7 +28,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author' => 'Thomas Hackl',
     'author_email' => 'thomas.hackl@uni-passau.de',
     'author_company' => 'University of Passau',
-    'version' => '3.3.3.0',
+    'version' => '3.3.3.1',
     'autoload' => array(
         'classmap' => array(
             'Classes',
